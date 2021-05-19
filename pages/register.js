d67a0bea4ec74865c4480eeb1b66dc0fa2dd50f9
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-05-19 16:58:12 +0530

    Axios post for resister

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState("Arif");
+  const [email, setEmail] = useState("arif@gmail.com");
+  const [password, setPassword] = useState("1234567");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.table(name, email, password);
+    //console.table(name, email, password);
+    const { data } = await axios.post(`http://localhost:8000/api/register`, {
+      name,
+      email,
+      password,
+    });
+    console.log("Register res--->", data);
   };
   return (
     <>
